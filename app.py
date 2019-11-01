1f2079500a2aed51cfb37610fc1ed5baff42d725
matthewcornell <matt@matthewcornell.org>
2019-11-01 17:17:46 -0400

    error handling

--- app.py
+++ app.py
@@ -20,9 +20,13 @@ def app(song_list_file):
     results = []  # 3-tuples: (line, title, video_url)
     for line in song_list_file:
         line = line.strip()
-        video_id, title = youtube_search_first_hit(line)
-        video_url = f'https://www.youtube.com/watch?v={video_id}'
-        results.append((line, title, video_url))
+        try:
+            video_id, title = youtube_search_first_hit(line)
+            video_url = f'https://www.youtube.com/watch?v={video_id}'
+            results.append((line, title, video_url))
+        except RuntimeError as rte:
+            click.echo(f"line=`{line}` got error: {rte}", err=True)
+            pass
     for line, title, video_url in results:
         click.echo(f"{line}\t{title}\t{video_url}")
 
@@ -41,19 +45,16 @@ def youtube_search_first_hit(query):
     # limit the fields returned to minimize quota impact. we only want:
     # - items > id > videoId
     # - items > snippet > title
-    fields = 'items(id,snippet(title))'
-    search_url = f'{api_url}?part=snippet&fields={fields}&maxResults={max_results}&q={encoded_query}&type=video&key={API_KEY}'
+    search_url = f'{api_url}?part=snippet&fields=items(id,snippet(title))&maxResults={max_results}&q={encoded_query}&type=video&key={API_KEY}'
     headers = {'Accept': 'application/json'}
     response = requests.get(search_url, headers=headers)
     response_json = response.json()
     if response.status_code != 200:
-        click.echo('error!', response, response_json['error'], err=True)
-        return
+        raise RuntimeError(f"error={response_json['error']}")
 
     response_items = response_json['items']  # up to max_results of them
     if not response_items:
-        click.echo('no items!', err=True)
-        return
+        raise RuntimeError("no hits")
 
     video_id = response_items[0]['id']['videoId']
     title = response_items[0]['snippet']['title']
