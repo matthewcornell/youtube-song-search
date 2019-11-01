0f647d0e575f881e07d8b1aec57d33038e204197
matthewcornell <matt@matthewcornell.org>
2019-11-01 17:00:44 -0400

    switch to YouTube search API

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-import re
-import time
 import urllib
 
 import click
 import requests
 
 
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-}
-SLEEP_SECONDS = 2
+API_KEY = 'AIzaSyAqckyDHpkDcDIz3A_Zvf2xQfJFrxg6ozk'
 
 
 @click.command()
@@ -17,64 +12,48 @@ SLEEP_SECONDS = 2
 def app(song_list_file):
     """
     Iterates through song/artist lines in song_list_file and prints a line for each one that includes the first
-    matching youtube video url at then end after a tab character. Fragile b/c scrapes the youtube search url rather than
-    using an API, etc.
+    matching youtube video url.
 
     :param song_list_file: file with one line for each song/artist, e.g., "ROLLING IN THE DEEP	Adele"
     """
-    print(f"song_list_file={song_list_file}")
-    results = []
+    click.echo(f"song_list_file={song_list_file}")
+    results = []  # 3-tuples: (line, title, video_url)
     for line in song_list_file:
         line = line.strip()
-        query = line  # didn't always work that well: query = f'song {line}'
-        urls = youtube_urls_for_query(query)
-        time.sleep(SLEEP_SECONDS)
-        first_url = urls[0] if urls else None
-
-        # todo title is probably aleady in youtube_urls_for_query() html:
-        # page_title = page_title_for_url(first_url) if first_url else '<no urls>'
-        # time.sleep(SLEEP_SECONDS)
-
-        # results.append(f"{line}\t{first_url or '<no urls>'}\t{page_title}")
-        results.append(f"{line}\t{first_url or '<no urls>'}")
-    print()
-    for result in results:
-        print(result)
+        video_id, title = youtube_search_first_hit(line)
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+        results.append((line, title, video_url))
+    for line, title, video_url in results:
+        click.echo(f"{line}\t{title}\t{video_url}")
 
 
-def youtube_urls_for_query(query):
+def youtube_search_first_hit(query):
     """
-    Scrapes youtube search results for a query and returns matching URLs in a list.
+    Calls the youtube search API to get the first hit for `query`.
 
-    :param query: song title, possibly with artist, etc. - passed to youtube/results. ex: "ROLLING IN THE DEEP	Adele"
-    :return: list of youtube URLs matching query. they are ordered according the search HTML
+    :param query: free-form text that should include a song title at a minimum, but including artist will yield better
+        results
+    :return: 2-tuple: (video_id, title) of the first matching hit
     """
     encoded_query = urllib.parse.quote(query)
+    api_url = 'https://www.googleapis.com/youtube/v3/search'
+    max_results = 1  # default = 5
+    search_url = f'{api_url}?part=snippet&maxResults={max_results}&q={encoded_query}&type=video&key={API_KEY}'
+    headers = {'Accept': 'application/json'}
+    response = requests.get(search_url, headers=headers)
+    response_json = response.json()
+    if response.status_code != 200:
+        click.echo('error!', response, response_json['error'], err=True)
+        return
 
-    # ex: "ROLLING IN THE DEEP	Adele" -> https://www.youtube.com/results?search_query=ROLLING+IN+THE+DEEP%09Adele
-    query_url = f"https://www.youtube.com/results?search_query={encoded_query}"
+    response_items = response_json['items']  # up to max_results of them
+    if not response_items:
+        click.echo('no items!', err=True)
+        return
 
-    print(f"getting: {query_url!r}")
-    response = requests.get(query_url, headers=HEADERS)
-    html = response.content.decode('utf-8')
-    data_context_item_ids = re.findall(r'data-context-item-id="(.*?)"', html)  # non-greedy
-    print(
-        f"  -> {len(html)} bytes, {len(data_context_item_ids)} data-context-item-ids: {data_context_item_ids}")
-    if len(html) < 5000:  # Our systems have detected unusual traffic from your computer network.
-        print('drat!', html)
-    return [f'https://www.youtube.com/watch?v={data_context_item_id}' for data_context_item_id in data_context_item_ids]
-
-
-def page_title_for_url(url):
-    response = requests.get(url, headers=HEADERS)
-    html = response.content.decode('utf-8')
-    # per https://stackoverflow.com/questions/26812470/how-to-get-page-title-in-requests
-    title = re.search('(?<=<title>).+?(?=</title>)', html, re.DOTALL).group().strip()
-    youtube_loc = title.find(' - YouTube')
-    if youtube_loc != -1:
-        return title[:-len(' - YouTube')]
-    else:
-        return title  # todo warn?
+    video_id = response_items[0]['id']['videoId']
+    title = response_items[0]['snippet']['title']
+    return video_id, title
 
 
 if __name__ == '__main__':
